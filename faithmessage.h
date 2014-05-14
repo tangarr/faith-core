b0bfab6e24c96106325edc20315f2b12b41d3f41
tangarr <tangarr@onet.pl>
2014-05-14 11:20:46 +0200

    zmiany

--- faithmessage.h
+++ faithmessage.h
@@ -4,6 +4,7 @@
 #include <QtCore>
 #include "faithcore.h"
 #include <QTcpSocket>
+#include "faithobject.h"
 
 
 class FaithMessage
@@ -11,12 +12,14 @@ class FaithMessage
 private:
     FaithMessage() {}
     Faithcore::MessageCode messageCode;
+    FaithObject *data;
 
 
 public:
     bool send(QTcpSocket*) const;
     bool recive(QTcpSocket*);
     Faithcore::MessageCode getMessageCode() const;
+    FaithObject* getData() const;
 };
 
 #endif // FAITHMESSAGE_H
